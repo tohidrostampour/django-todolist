900b434e55ddbba9ce20c2cdcf7999019d7d15f5
nimilioum <nimilioum@gmail.com>
2021-10-10 18:54:33 +0330

    fix: static_root problem

--- config/settings.py
+++ config/settings.py
@@ -111,6 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "todolist/static"
+STATIC_ROOT = os.path.join(BASE_DIR, "todolist/static")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'

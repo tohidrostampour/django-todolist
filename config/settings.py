b2f90619c61d63f77541faab4ec89dc264a297d3
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2021-10-11 10:16:47 +0330

    Styling pages

--- config/settings.py
+++ config/settings.py
@@ -112,5 +112,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 

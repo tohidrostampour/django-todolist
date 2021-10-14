28a893b2401c3d762e2b2f39aacad70ca49c74e4
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2021-10-14 22:31:37 +0330

    set default for color input

--- todolist/static/todolist/theme.js
+++ todolist/static/todolist/theme.js
@@ -1,11 +1,20 @@
 let menu = document.querySelector('.theme-container');
+let Background = document.querySelectorAll(".input-field input");
 
 document.addEventListener('DOMContentLoaded', () => {
     let items = readFromLocalStorage('background');
     setElementColor(items);
+    Background[0].value = items[0];
+    Background[1].value = items[1];
+    Background[2].value = items[2];
+    Background[3].value = items[3];
+    Background[4].value = items[4];
+    Background[5].value = items[5];
+    Background[6].value = items[6];
+    Background[7].value = items[7];
 });
 
-let Background = document.querySelectorAll(".input-field input");
+
 
 document.querySelector('.change-theme').addEventListener('click', function () {
     let backgroundColor = [Background[0].value, Background[1].value, Background[2].value, Background[3].value, Background[4].value, Background[5].value, Background[6].value, Background[7].value];
@@ -13,6 +22,8 @@ document.querySelector('.change-theme').addEventListener('click', function () {
     setElementColor(backgroundColor);
 
     localStorage.setItem('background', backgroundColor);
+
+    menu.classList.remove('active');
 
 })
 
@@ -25,16 +36,15 @@ themeOpen.onclick = function () {
 
 function readFromLocalStorage(key) {
     let items;
-    if(localStorage.getItem(key) == null) {
+    if (localStorage.getItem(key) == null) {
         items = [];
-    }
-    else {
+    } else {
         items = localStorage.getItem(key).split(',');
     }
     return items;
 }
 
-function setElementColor(colors){
+function setElementColor(colors) {
     document.querySelector(':root').style.setProperty('--main-color', colors[0]);
     document.querySelector(':root').style.setProperty('--main-box', colors[1]);
     document.querySelector(':root').style.setProperty('--title-color', colors[2]);

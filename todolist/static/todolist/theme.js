2c5e18f08ac50000e6cd40810c050a8d8bde0fb9
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2021-10-14 10:01:29 +0330

    create theme part

---
+++ todolist/static/todolist/theme.js
@@ -0,0 +1,45 @@
+let themeOpen = document.querySelector('.head-content .theme-open');
+let menu = document.querySelector('.theme-container');
+
+console.log('hi');
+
+themeOpen.onclick = function () {
+    menu.classList.toggle('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let items;
+
+    if (localStorage.getItem('background') === null) {
+        items = [];
+    } else {
+        items = localStorage.getItem('background').split(',');
+    }
+
+    document.querySelector(':root').style.setProperty('--main-background', items[0]);
+    document.querySelector(':root').style.setProperty('--main-box', items[1]);
+    document.querySelector(':root').style.setProperty('--title-color', items[2]);
+    document.querySelector(':root').style.setProperty('--lable-color', items[3]);
+    document.querySelector(':root').style.setProperty('--btn-color', items[4]);
+    document.querySelector(':root').style.setProperty('--btn-text', items[5]);
+    document.querySelector(':root').style.setProperty('--btn-hover', items[6]);
+    document.querySelector(':root').style.setProperty('--btn-text-hover', items[7]);
+});
+
+let Background = document.querySelectorAll(".input-field input");
+
+document.querySelector('.change-theme').addEventListener('click', function () {
+    document.querySelector(':root').style.setProperty('--main-background', Background[0].value);
+    document.querySelector(':root').style.setProperty('--main-box', Background[1].value);
+    document.querySelector(':root').style.setProperty('--title-color', Background[2].value);
+    document.querySelector(':root').style.setProperty('--lable-color', Background[3].value);
+    document.querySelector(':root').style.setProperty('--btn-color', Background[4].value);
+    document.querySelector(':root').style.setProperty('--btn-text', Background[5].value);
+    document.querySelector(':root').style.setProperty('--btn-hover', Background[6].value);
+    document.querySelector(':root').style.setProperty('--btn-text-hover', Background[7].value);
+
+    let backgroundColor = [Background[0].value, Background[1].value, Background[2].value, Background[3].value, Background[4].value, Background[5].value, Background[6].value, Background[7].value];
+
+    localStorage.setItem('background', backgroundColor);
+
+})

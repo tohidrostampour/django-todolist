7392b960b423c73f6b3f6d5dcc708aaa4b2a32ab
twhidrst <rostampourtwhid@gmail.com>
2021-10-14 16:47:02 +0330

    added ordering tasks by priority

--- todolist/models.py
+++ todolist/models.py
@@ -7,21 +7,20 @@ from taggit.managers import TaggableManager
 
 
 class Task(models.Model):
-    class Priority(models.TextChoices):
-        LOW = 'L', 'Low'
-        Medium = 'M', 'Medium'
-        High = 'H', 'High'
+    class Priority(models.IntegerChoices):
+        LOW = 1, 'Low'
+        Medium = 2, 'Medium'
+        High = 3, 'High'
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     created = models.DateTimeField(default=timezone.now)
     due_date = models.DateTimeField(blank=True, null=True)
     completed = models.BooleanField(default=False)
     tags = TaggableManager(blank=True)
-    priority = models.CharField(
-        max_length=1, choices=Priority.choices, default=Priority.LOW)
+    priority = models.PositiveIntegerField(choices=Priority.choices, default=Priority.LOW)
 
     class Meta:
-        ordering = ('-created',)
+        ordering = ('priority',)
 
     def __str__(self) -> str:
         return self.title

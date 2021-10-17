43e551d8feb7d8c33feb6c7b75531e55cf06dc90
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2021-10-17 18:21:44 +0330

    create default themes part

--- todolist/static/todolist/theme.js
+++ todolist/static/todolist/theme.js
@@ -28,18 +28,54 @@ let menu = document.querySelector('.theme-container');
 let menu2 = document.querySelector('.theme-container2');
 
 let Background = document.querySelectorAll(".input-field-c input");
+let defaultBackgroundColor = document.querySelectorAll(".theme-container2 div");
 
 let themeOpen = document.querySelector('.head-content .theme-open');
 
 themeOpen.onclick = function () {
     menu.classList.toggle('active');
+    menu2.classList.remove('active');
 }
 
 let themeOpen2 = document.querySelector('.head-content .theme-open2');
 
 themeOpen2.onclick = function () {
     menu2.classList.toggle('active');
+    menu.classList.remove('active');
 }
+
+
+defaultBackgroundColor[0].addEventListener('click', function(){
+    colors = ['#080d0c','#2c403a','#b4d9d9','#9fbfbf','#8aa69e','#141414','#b8adb1','#ffffff'];
+    setElementColor(colors);
+    localStorage.setItem('background', colors);
+});
+defaultBackgroundColor[1].addEventListener('click', function(){
+    colors = ['#0d0d0d','#404040','#d9d9d9','#a6a6a6','#737373','#ffffff','#7c7979','#d9d9d9'];
+    setElementColor(colors);
+    localStorage.setItem('background', colors);
+});
+defaultBackgroundColor[2].addEventListener('click', function(){
+    colors = ['#202426','#4b4b49','#6c733d','#f2f2f2','#9da65d','#000000','#6c733d','#ffffff'];
+    setElementColor(colors);
+    localStorage.setItem('background', colors);
+});
+defaultBackgroundColor[3].addEventListener('click', function(){
+    colors = ['#323c73','#5362b2','#d98c48','#ffffff','#d98c4a','#ffffff','#d9b1a3','#000000'];
+    setElementColor(colors);
+    localStorage.setItem('background', colors);
+});
+defaultBackgroundColor[4].addEventListener('click', function(){
+    colors = ['#2e4559','#4a6d8c','#073c5f','#1a354d','#a9c6d9','#000000','#6a8aa6','#ffffff'];
+    setElementColor(colors);
+    localStorage.setItem('background', colors);
+});
+defaultBackgroundColor[5].addEventListener('click', function(){
+    colors = ['#400106','#8b0e27','#f2bdc7','#ffffff','#f2bdc7','#000000','#ec748c','#fcfcfc'];
+    setElementColor(colors);
+    localStorage.setItem('background', colors);
+});
+
 
 function readFromLocalStorage(key) {
     let items;
@@ -72,3 +108,5 @@ function setElementColor(colors) {
         document.querySelector(':root').style.setProperty('--btn-text-hover', '#8ab0ca');
     }
 }
+
+
